de053f74e49abe095b69a46d7f363b14f8065d26
Nathan Cavalcante <105390203+N4teC@users.noreply.github.com>
2024-02-22 20:29:22 +0000

    create atv3

---
+++ Aula_01/Lista_03/atv3.py
@@ -0,0 +1,9 @@
+print('Digite a nota do primeiro bimestre da disciplina:')
+a = int(input())
+
+print('Digite a nota do segundo bimestre da disciplina:')
+b = int(input())
+
+media = ((a*2)+(b*3))//(2+3)
+
+print('Média parcial =', media)

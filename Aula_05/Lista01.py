942d83e32a62c64609a053fe94d2f6e10be0489d
Nathan Cavalcante <105390203+N4teC@users.noreply.github.com>
2024-03-07 17:47:47 -0300

    Update Lista01.py

--- Aula_05/Lista01.py
+++ Aula_05/Lista01.py
@@ -4,26 +4,31 @@
 
 import math
 
+# Model
 class Circulo:
-    def __init__(self):
-        self.raio = 0
+    def __init__(self, raio):
+        self.set_raio(raio)
+    def get_raio(self):
+        return self.__raio
+    def set_raio(self, raio):
+        self.__raio = raio
+        if raio < 0:
+            raise ValueError()
     def calc_area(self):
-        area = math.pi * self.raio * self.raio
+        area = math.pi * self.__raio * self.__raio
         return area
     def calc_circunferencia(self):
-        circ = 2 * math.pi * self.raio
+        circ = 2 * math.pi * self.__raio
         return circ
     
-# main:
-circulo = Circulo()
-
-circulo.raio = int(input('Digite o valor do raio do círculo: '))
+# UI
+circulo = Circulo(int(input('Digite o valor do raio do círculo: ')))
 
 while True:
     x = int(input('Digite 0 para inserir outro valor de raio, 1 para saber a área do círculo, 2 para a circunferência, ou 3 para sair: '))
     
     if x == 0:
-        circulo.raio = int(input('Digite o novo valor do raio: '))
+        circulo.set_raio(int(input('Digite o novo valor do raio: ')))
     elif x == 1:
         print('Área do Circulo:', circulo.calc_area())
     elif x == 2:
